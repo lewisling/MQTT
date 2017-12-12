60e1e5031dc859ff6dfa13a4c8c5d46f65fb6da3
xlsn0w <xlsn0w@outlook.com>
2017-12-12 14:32:23 +0800

    init

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+use_frameworks!
+
+target 'XLsn0wMQTT' do
+    
+pod 'MQTTClient'
+pod 'CocoaMQTT'
+
+end
+
---
+++ XLsn0wMQTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLsn0wMQTT
+//
+//  Created by golong on 2017/12/12.
+//  Copyright © 2017年 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
